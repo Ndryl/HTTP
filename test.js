b000474af243d4600b1e7bbd421704122dc7bbc9
Ndryl <hendrylmr@outlook.com>
2025-02-25 20:01:32 -0300

    FEAT: Usando async/await

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
-function fetchPokemon(pokemon) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+// function fetchPokemon(pokemon) {
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+//       .then((response) => response.json())
+//       .then((data) => console.log(data))
+//       .catch((err) => console.log(err));
+// }
+//FORMA MAIS USUAL:
+async function fetchPokemon(pokemon){
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 // Chamando a função
